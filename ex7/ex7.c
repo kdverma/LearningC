4df0d8949f53d322394d750cf610955add0599a4
kdverma <writetokapildev@gmail.com>
2016-07-06 14:22:55 -0400

    adding ex7

---
+++ ex7/ex7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (int argc, char **argv)
+{
+    char a = '\0';
+    printf("\n a = [%c]",a);
+    printf("\n a = [%s]",a);
+    return 0;
+}
+
+/*
+OUTPUT
+[training@localhost ex7]$ ./ex7
+
+ a = []
+ a = [(null)][training@localhost ex7]$
+*/
